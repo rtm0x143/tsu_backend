2a5ddd8086336e77dde213c08401c475c9d97217
rtm0x143 <russia-artyom@mail.ru>
2022-10-24 23:35:36 +0700

    authentication policy to check token for rejected

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 using MovieCatalogBackend.Data.MovieCatalog;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using MovieCatalogBackend.Data.Tokens;
 using MovieCatalogBackend.Helpers;
+using MovieCatalogBackend.Services;
+using MovieCatalogBackend.Services.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationHelper.BaseConfiguration = builder.Configuration;
 
 builder.Services.AddControllers();
+
+var authenticationOptions = new AuthenticationOptions(builder.Configuration);
+var serverUrls = builder.WebHost.GetSetting(WebHostDefaults.ServerUrlsKey)?.Split(";").ToHashSet();
+if (serverUrls != null)
+{
+    authenticationOptions.Issuers.UnionWith(serverUrls);
+    authenticationOptions.Audiences.UnionWith(serverUrls);
+}
+
+builder.Services
+    .AddSingleton(authenticationOptions) 
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+        options.TokenValidationParameters = authenticationOptions.CreateValidationParameters());
+
+builder.Services
+    .AddScoped<ITokenService, TokenService>()
+    .AddSingleton<TokenListCleanerDemon>()
+    .AddScoped<IAuthorizationHandler, TokenNotBlackedAuthorizationHandler>()
+    .AddAuthorization(options =>
+        options.AddPolicy("TokenNotBlacked", policyBuilder => policyBuilder.AddRequirements(TokenNotBlackedRequirements.Istance))
+    );
 
 builder.Services.AddEndpointsApiExplorer()
     .AddSwaggerGen()
@@ -19,6 +45,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Startup cleaner  
+app.Services.GetService<TokenListCleanerDemon>();
 
 app.UseHttpsRedirection();
 app.UseRouting();
---
+++ Services/Authentication/TokenNotBlackedAuthorization.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MovieCatalogBackend.Services.Authentication;
+
+public class TokenNotBlackedRequirements : IAuthorizationRequirement
+{
+    protected TokenNotBlackedRequirements() { }
+    public static TokenNotBlackedRequirements Istance = new();
+}
+
+public class TokenNotBlackedAuthorizationHandler : AuthorizationHandler<TokenNotBlackedRequirements, HttpContext>
+{
+    ITokenService _tokenService;
+
+    public TokenNotBlackedAuthorizationHandler(ITokenService tokenService) =>
+        _tokenService = tokenService;
+
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, TokenNotBlackedRequirements requirement, HttpContext resource)
+    {
+        var token = await resource.GetTokenAsync("access_token");
+        if (token is null) context.Fail();
+        else if (await _tokenService.IsTokenRejected(token))
+            context.Fail(new AuthorizationFailureReason(this, "Token is rejected, that could mean that session had logged out"));
+        else context.Succeed(requirement);
+    }
+}

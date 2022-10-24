555e35200176b5e9c789c54aa6789be71f47c42f
rtm0x143 <russia-artyom@mail.ru>
2022-10-24 23:27:48 +0700

    TokenListCleanerDemon; service for cleaning expired tokens in backed thread

---
+++ Services/TokenListCleanerDemon.cs
@@ -0,0 +1,59 @@
+﻿using MovieCatalogBackend.Data.Tokens;
+
+namespace MovieCatalogBackend.Services;
+
+public class TokenListCleanerDemon : IDisposable
+{
+    private ILogger _logger;
+    private Thread _demon;
+    public bool _interruptionFlag = false; 
+    private Mutex _cleanupRateMutex = new Mutex(true);
+
+    public TimeSpan CleanRate { get; set; } = TimeSpan.FromMinutes(1);
+    public bool Disposed { get; private set; } = false;
+
+    public TokenListCleanerDemon(ILogger<TokenListCleanerDemon> logger)
+    {
+        _logger = logger;
+        _demon = new Thread(_demonRuntime);
+        _demon.Start();
+    }
+    
+    ~TokenListCleanerDemon() => Dispose();
+
+    private void _demonRuntime()
+    {
+        while (!_interruptionFlag)
+        {
+            while (!_interruptionFlag)
+            {
+                try
+                {
+                    using var tokenListContext = new TokenListContext();
+                    var tokens = tokenListContext.Tokens.ToArray();
+                    tokenListContext.Tokens.RemoveRange(
+                        tokenListContext.Tokens.Where(t => t.Expiretion < DateTime.UtcNow));
+                    tokenListContext.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Occured while cleaning TokenList database from expired tokens");
+                }
+
+                _cleanupRateMutex.WaitOne(CleanRate);
+            }
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (Disposed) return;
+        _interruptionFlag = true;
+        _cleanupRateMutex.Dispose();
+        _demon.Join();  
+        GC.SuppressFinalize(this);
+        Disposed = true;
+    }
+}
